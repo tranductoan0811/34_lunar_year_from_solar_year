5f8414197ac81b30f4f8427f1613f957a865c352
tranductoan <tranductoan0811@gmail.com>
2022-06-28 21:30:04 +0700

    update

---
+++ lunar_year_from_solar_year.js
@@ -0,0 +1,87 @@
+function lunaryear() {
+  let year, can, chi, display;
+  year = parseInt(document.getElementById("solar_year").value);
+  can = '';
+  chi = '';
+  display = '';
+
+  // xác định can
+  switch (year % 10) {
+    case 0:
+      can = "Canh";
+      break;
+    case 1:
+      can = "Tân";
+      break;
+    case 2:
+      can = "Nhâm";
+      break;
+    case 3:
+      can = "Quý";
+      break;
+    case 4:
+      can = "Giáp";
+      break;
+    case 5:
+      can = "Ất";
+      break;
+    case 6:
+      can = "Bính";
+      break;
+    case 7:
+      can = "Đinh";
+      break;
+    case 8:
+      can = "Mậu";
+      break;
+    case 9:
+      can = "Kỷ";
+      break;
+  }
+
+// xác định chi
+switch (year % 12) {
+  case 0:
+    chi = "Thân";
+    break;
+  case 1:
+    chi = "Dậu";
+    break;
+  case 2:
+    chi = "Tuất";
+    break;
+  case 3:
+    chi = "Hợi";
+    break;
+  case 4:
+    chi = "Tý";
+    break;
+  case 5:
+    chi = "Sửu";
+    break;
+  case 6:
+    chi = "Dần";
+    break;
+  case 7:
+    chi = "Mão";
+    break;
+  case 8:
+    chi = "Thìn";
+    break;
+  case 9:
+    chi = "Tỵ";
+    break;
+  case 10:
+    chi = "Ngọ";
+    break;
+  case 11:
+    chi = "Mùi";
+    break;
+
+}
+
+display = ("Năm âm lịch tương ứng với năm dương lịch: " + parseInt(year) + " là năm " + can + " " + chi);
+
+document.getElementById("result").innerHTML = display;
+
+}
